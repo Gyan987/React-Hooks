148017e491ac17ff3652e6bdbd99d9fc2d64ab4f
Gyan <gyanprakasht987@gmail.com>
2026-07-11 20:01:54 +0530

    Navbar .

--- React-Hooks/src/App.jsx
+++ React-Hooks/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -25,6 +26,7 @@ function App() {
 
   return (
     <>
+    <Navbar color={"navy" + "blue"}/>
       <section id="center">
         <div className="hero">
           <img src={heroImg} className="base" width="170" height="179" alt="" />
