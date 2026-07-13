7e2d35047148ee7e5dff88b7ec5835cc4868bbfb
Gyan <gyanprakasht987@gmail.com>
2026-07-13 18:36:19 +0530

    modified:   React-Hooks/src/App.css

---
+++ React-Hooks/src/components/Header.jsx
@@ -0,0 +1,18 @@
+function Header() {
+
+
+
+
+    return(
+        <header>
+            <h1>My Website App</h1>
+            <nav>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+            </nav>
+        </header>
+    );
+}
+
+export default Header
